0e73d4a044a6ccac2e90a6ac0cfd5dfd501a39d2
jahjahyassine <jahjah.yassine@ensam-casa.ma>
2026-06-23 16:48:53 +0100

    Add Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,112 @@
+import "../style.css"
+
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import Side from "/home/yassine/Projects/GymTracker/src/assets/side.jpg"
+
 function Login() {
+
+    const URL_BASE = import.meta.env.VITE_BACKEND_URL
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [rememberOption, setRememberOption] = useState(false)
+    const [userData, setUserData] = useState({})
+
+
+    async function handleSubmit() {
+        e.preventDefault();
+
+        const validationResponse = await fetch(
+            `${URL_BASE}`,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: { email },
+                    password: { password }
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }
+        )
+
+        if (!validationResponse.ok) throw new Error("Incorrect Credentials")
+
+        const data = await validationResponse.json()
+    }
+
     return (
-        <div>cc hh</div>
+        <main className="relative h-screen flex justify-center items-center font-sora">
+            <div className="fixed -z-10 inset-0 h-full w-full bg-gradient-to-tr from-brand to-bg-primary" />
+
+            <section className="border border-stroke lg:h-[90vh] md:h-[50vh] w-[80vw] lg:grid lg:grid-cols-2 items-center overflow-hidden p-10 rounded-2xl gap-4 bg-text-primary/10 backdrop-blur-xl md:flex md:justify-center">
+
+                <div className="text-text-primary p-8 flex flex-col ">
+                    <div className="text-center">
+                        <h2 className="font-bold text-4xl tracking-wide">Welcome back, <span className="bg-gradient-to-l from-brand-soft to-white bg-clip-text text-transparent">Champ</span></h2>
+                        <p className="text-text-muted">Your progress doesn’t stop here. Let’s make today count.</p>
+                    </div>
+
+
+                    <form
+                        className="flex flex-col p-10 gap-6" onSubmit={handleSubmit}
+                    >
+                        <div className="grid grid-cols-3 gap-4 text-lg">
+                            <label htmlFor="email" className="col-span-1 font-medium">Email Address</label>
+                            <input type="email" id="email" value={email}
+                                className="col-span-2  border border-stroke bg-brand-soft/10 backdrop-blur-md px-2 py-1 rounded-lg
+                                focus:outline-none
+focus:border-brand-soft
+focus:ring-2
+focus:ring-brand-soft/20"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-3 gap-4 text-lg">
+                            <label htmlFor="password" className="col-span-1 font-medium">Password</label>
+                            <input type="password" id="password" value={password}
+                                className="col-span-2 border border-stroke bg-brand-soft/10 backdrop-blur-md px-2 py-1 rounded-lg
+                                focus:outline-none
+focus:border-brand-soft
+focus:ring-2
+focus:ring-brand-soft/20"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="text-text-muted flex justify-between">
+                            <div>
+                                <input type="checkbox" checked={rememberOption} id="rememberOption"
+                                    onChange={e => setRememberOption(e.target.checked)}
+
+                                />
+                                <label htmlFor="rememberOption"> Remember me</label>
+                            </div>
+
+
+                            <p className="hover:text-text-primary hover:cursor-pointer duration-300 ease-in-out">Forgot password?</p>
+                        </div>
+
+                        <button type="submit" className="bg-brand-soft text-lg p-1 rounded-lg hover:bg-brand-soft/80 duration-300 ease-in-out font-medium">Login</button>
+
+                        <p className="text-center text-sm text-text-muted font-semibold tracking-wider">Don’t have an account → <Link to='/register' className="hover:text-text-primary duration-300 ease-in-out">Register</Link></p>
+                    </form>
+                </div>
+
+                <div className="h-full grid overflow-hidden rounded-xl md:hidden lg:block">
+                   <img
+    src={Side}
+    className="w-full h-full object-cover"
+/>
+                </div>
+
+
+
+            </section>
+        </main>
     )
 }
 
